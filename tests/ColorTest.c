7c205f1a26366d3b065a10e06727767f348d212d
Brendan <brewinn@protonmail.com>
2021-07-11 12:53:19 -0500

    [refactor] Remove duplication in ColorTest

--- tests/ColorTest.c
+++ tests/ColorTest.c
@@ -3,6 +3,9 @@
 
 TEST_GROUP(Color);
 
+static Color color;
+static Color color1;
+static Color color2;
 
 TEST_SETUP(Color)
 {
@@ -12,30 +15,31 @@ TEST_TEAR_DOWN(Color)
 {
 }
 
+void AssertColorEquals(float red, float green, float blue)
+{
+	TEST_ASSERT_EQUAL_FLOAT(red, color.red);
+	TEST_ASSERT_EQUAL_FLOAT(green, color.green);
+	TEST_ASSERT_EQUAL_FLOAT(blue, color.blue);
+}
+
 TEST(Color, ColorIsThreeTuple)
 {
-	Color color = Color_Create(-0.5, 0.4, 1.7);
-	TEST_ASSERT_EQUAL_FLOAT(-0.5, color.red);
-	TEST_ASSERT_EQUAL_FLOAT(0.4, color.green);
-	TEST_ASSERT_EQUAL_FLOAT(1.7, color.blue);
+	color = Color_Create(-0.5, 0.4, 1.7);
+	AssertColorEquals(-0.5, 0.4, 1.7);
 }
 
 TEST(Color, ColorAdditionYieldsAnotherColor)
 {
-	Color color1 = Color_Create(0.9, 0.6, 0.75);
-	Color color2 = Color_Create(0.7, 0.1, 0.25);
-	Color colorSum = Color_Add(color1, color2);
-	TEST_ASSERT_EQUAL_FLOAT(1.6, colorSum.red);
-	TEST_ASSERT_EQUAL_FLOAT(0.7, colorSum.green);
-	TEST_ASSERT_EQUAL_FLOAT(1.0, colorSum.blue);
+	color1 = Color_Create(0.9, 0.6, 0.75);
+	color2 = Color_Create(0.7, 0.1, 0.25);
+	color  = Color_Add(color1, color2);
+	AssertColorEquals(1.6, 0.7, 1.0);
 }
 
 TEST(Color, ColorSubtractionYieldsAnotherColor)
 {
-	Color color1 = Color_Create(0.9, 0.6, 0.75);
-	Color color2 = Color_Create(0.7, 0.1, 0.25);
-	Color colorSum = Color_Subtract(color1, color2);
-	TEST_ASSERT_EQUAL_FLOAT(0.2, colorSum.red);
-	TEST_ASSERT_EQUAL_FLOAT(0.5, colorSum.green);
-	TEST_ASSERT_EQUAL_FLOAT(0.5, colorSum.blue);
+	color1 = Color_Create(0.9, 0.6, 0.75);
+	color2 = Color_Create(0.7, 0.1, 0.25);
+	color = Color_Subtract(color1, color2);
+	AssertColorEquals(0.2, 0.5, 0.5);
 }
