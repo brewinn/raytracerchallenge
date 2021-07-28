c849b9dcf0ed4e22dc4d3cbe85ddb54c4d94eb70
Brendan <brewinn@protonmail.com>
2021-07-27 20:14:28 -0500

    [test] Complete test suite for translations
    Added two more tests, showing the effect of an inverse translation and
    the lack of an effect on vectors.

--- tests/TransformationsTest.c
+++ tests/TransformationsTest.c
@@ -14,12 +14,37 @@ TEST_TEAR_DOWN(Transformations)
 
 TEST(Transformations, TranslationMovesAPoint)
 {
-	Tuple point = Tuple_CreatePoint(5, -3, 2);
-	Matrix translation = Transformation_Translation(-3, 4, 5);
+	Tuple point = Tuple_CreatePoint(-3, 4, 5);
+	Matrix translation = Transformation_Translation(5, -3, 2);
 	point = Matrix_MultiplyTuple(translation, point);
 	Matrix_Destroy(&translation);
 
 	TEST_ASSERT_EQUAL_FLOAT(2, point.x);
 	TEST_ASSERT_EQUAL_FLOAT(1, point.y);
 	TEST_ASSERT_EQUAL_FLOAT(7, point.z);
+}
+
+TEST(Transformations, InverseTranslationMovesPointInOtherDirection)
+{
+	Tuple point = Tuple_CreatePoint(-3, 4, 5);
+	Matrix translation = Transformation_Translation(5, -3, 2);
+	Matrix_Copy(translation, Matrix_Inverse(translation));
+	point = Matrix_MultiplyTuple(translation, point);
+	Matrix_Destroy(&translation);
+
+	TEST_ASSERT_EQUAL_FLOAT(-8, point.x);
+	TEST_ASSERT_EQUAL_FLOAT(7, point.y);
+	TEST_ASSERT_EQUAL_FLOAT(3, point.z);
+}
+
+TEST(Transformations, TranslationDoesNotAffectVectors)
+{
+	Tuple vector = Tuple_CreateVector(-3, 4, 5);
+	Matrix translation = Transformation_Translation(5, -3, 2);
+	vector = Matrix_MultiplyTuple(translation, vector);
+	Matrix_Destroy(&translation);
+
+	TEST_ASSERT_EQUAL_FLOAT(-3, vector.x);
+	TEST_ASSERT_EQUAL_FLOAT(4, vector.y);
+	TEST_ASSERT_EQUAL_FLOAT(5, vector.z);
 }
--- tests/TransformationsTestRunner.c
+++ tests/TransformationsTestRunner.c
@@ -3,4 +3,6 @@
 TEST_GROUP_RUNNER(Transformations)
 {
 	RUN_TEST_CASE(Transformations, TranslationMovesAPoint);
+	RUN_TEST_CASE(Transformations, InverseTranslationMovesPointInOtherDirection);
+	RUN_TEST_CASE(Transformations, TranslationDoesNotAffectVectors);
 }
