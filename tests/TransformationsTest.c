211c8d4a3483d6825269fd69187e89f56f2ee1a8
Brendan <brewinn@protonmail.com>
2021-07-27 22:32:26 -0500

    [refactor] Refactor translation tests
    Extracted out three static variables and a test assertion function.

--- tests/TransformationsTest.c
+++ tests/TransformationsTest.c
@@ -1,6 +1,9 @@
 #include "unity_fixture.h"
 #include "Transformations.h"
 
+static Tuple expected, actual;
+static Matrix transformation;
+
 TEST_GROUP(Transformations);
 
 TEST_SETUP(Transformations)
@@ -9,42 +12,43 @@ TEST_SETUP(Transformations)
 
 TEST_TEAR_DOWN(Transformations)
 {
+	Matrix_Destroy(&transformation);
 }
 
+static void AssertTuplesEqual()
+{
+	TEST_ASSERT_EQUAL_FLOAT(expected.x, actual.x);
+	TEST_ASSERT_EQUAL_FLOAT(expected.y, actual.y);
+	TEST_ASSERT_EQUAL_FLOAT(expected.z, actual.z);
+	TEST_ASSERT_EQUAL_FLOAT(expected.w, actual.w);
+}
 
 TEST(Transformations, TranslationMovesAPoint)
 {
 	Tuple point = Tuple_CreatePoint(-3, 4, 5);
-	Matrix translation = Transformation_Translation(5, -3, 2);
-	point = Matrix_MultiplyTuple(translation, point);
-	Matrix_Destroy(&translation);
-
-	TEST_ASSERT_EQUAL_FLOAT(2, point.x);
-	TEST_ASSERT_EQUAL_FLOAT(1, point.y);
-	TEST_ASSERT_EQUAL_FLOAT(7, point.z);
+	transformation = Transformation_Translation(5, -3, 2);
+	actual = Matrix_MultiplyTuple(transformation, point);
+	expected = Tuple_CreatePoint(2, 1, 7);
+	AssertTuplesEqual();
 }
 
 TEST(Transformations, InverseTranslationMovesPointInOtherDirection)
 {
 	Tuple point = Tuple_CreatePoint(-3, 4, 5);
 	Matrix translation = Transformation_Translation(5, -3, 2);
-	Matrix_Copy(translation, Matrix_Inverse(translation));
-	point = Matrix_MultiplyTuple(translation, point);
+	transformation = Matrix_Inverse(translation);
+	actual = Matrix_MultiplyTuple(transformation, point);
 	Matrix_Destroy(&translation);
-
-	TEST_ASSERT_EQUAL_FLOAT(-8, point.x);
-	TEST_ASSERT_EQUAL_FLOAT(7, point.y);
-	TEST_ASSERT_EQUAL_FLOAT(3, point.z);
+	expected = Tuple_CreatePoint(-8, 7, 3);
+	AssertTuplesEqual();
 }
 
 TEST(Transformations, TranslationDoesNotAffectVectors)
 {
 	Tuple vector = Tuple_CreateVector(-3, 4, 5);
-	Matrix translation = Transformation_Translation(5, -3, 2);
-	vector = Matrix_MultiplyTuple(translation, vector);
-	Matrix_Destroy(&translation);
-
-	TEST_ASSERT_EQUAL_FLOAT(-3, vector.x);
-	TEST_ASSERT_EQUAL_FLOAT(4, vector.y);
-	TEST_ASSERT_EQUAL_FLOAT(5, vector.z);
+	transformation = Transformation_Translation(5, -3, 2);
+	actual = Matrix_MultiplyTuple(transformation, vector);
+	expected = Tuple_CreateVector(-3, 4, 5);
+	AssertTuplesEqual();
 }
+
